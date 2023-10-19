7642fc5339864acaab611be878645cce45d39e32
Eden <edenlesik@gmail.com>
2023-10-19 18:09:26 +0900

    style main page layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,19 @@
 import MainContent from "@/components/MainContent";
+import CardContent from "@/components/CardContent";
 import styled from "styled-components";
 
 const Home = () => {
   return (
     <HomeContainer>
-      hello world
       <MainContent />
+      <CardContent />
     </HomeContainer>
   );
 };
 
-const HomeContainer = styled.main``;
+const HomeContainer = styled.main`
+  width: 100vw;
+  min-height: 100vh;
+`;
 
 export default Home;

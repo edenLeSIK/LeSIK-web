2a3632e8b7a0707045429f9ff975327a42d10161
EDEN <131233647+edenLeSIK@users.noreply.github.com>
2023-10-20 01:52:49 +0900

    Feature inquiry page (#7)

---
+++ src/constants/inquiry.js
@@ -0,0 +1,27 @@
+export const inquiryPageContentList = {
+  headline: "지금까지의 외식업의 고민을 덜어드립니다",
+  desc: "최초의 초개인화 푸드 서비스 프랜차이즈의 일원이 되어보세요!",
+  contents: [
+    {
+      content:
+        "모든 영업과 주문은 앱을 통해 이루어져요! 다른 걱정없이 생산에만 집중하세요.",
+    },
+    {
+      content:
+        "기존 주방을 활용하세요. 이미 보유하고 있는 시설에 시스템만 도입해 전환할 수 있어요.",
+    },
+    {
+      content:
+        "정말 쉬워요. 모든 재료는 블록단위로 제공되어 시스템에 따라 조합하여 생산돼요. ",
+    },
+  ],
+};
+
+export const formList = [
+  { id: 1, label: "이름", mark: true },
+  { id: 2, label: "전화번호", mark: true },
+  { id: 3, label: "이메일", mark: true },
+  { id: 4, label: "문의사항", mark: true },
+  { id: 5, label: "가맹점 이름", mark: true },
+  { id: 6, label: "가맹점 주소", mark: false },
+];

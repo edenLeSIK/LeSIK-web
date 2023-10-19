28c74a31f633b0382568da3943b0e0152a06172d
Eden <edenlesik@gmail.com>
2023-10-19 18:50:33 +0900

    feature Card content

---
+++ src/components/CardContent/Card.js
@@ -0,0 +1,95 @@
+import Image from "next/image";
+import styled from "styled-components";
+import { lightGray, white } from "@/styles/theme";
+
+const Card = ({ contents }) => {
+  return (
+    <Container>
+      {contents.map((el) => (
+        <div className="content-item" key={el.headline}>
+          <div className="icon">
+            <Image alt="icon" src={el.icon} width={100} height={100} />
+          </div>
+          <h3 className="sub-headline">{el.headline}</h3>
+          <p className="text">{el.text}</p>
+        </div>
+      ))}
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-auto-rows: auto;
+  column-gap: 2.22vw;
+  row-gap: 30px;
+  align-items: flex-start;
+  width: 50vw;
+
+  @media screen and (max-width: 939px) and (min-width: 767px),
+    screen and (max-width: 766px) {
+    width: 100%;
+    grid-template-columns: 1fr;
+    row-gap: 35px;
+  }
+
+  .content-item {
+    position: relative;
+    display: -webkit- flex;
+    display: flex;
+    -webkit-flex-direction: column;
+    flex-direction: column;
+    -webkit-align-items: center;
+    align-items: center;
+    -webkit-justify-content: flex-start;
+    justify-content: flex-start;
+    padding: 30px;
+    border: 1px solid ${lightGray};
+    background: ${white};
+
+    @media screen and (max-width: 939px) and (min-width: 767px),
+      screen and (max-width: 766px) {
+      margin-left: 5.5583333333vw;
+      margin-right: 5.5583333333vw;
+    }
+
+    .icon {
+      max-width: 120px;
+      height: auto;
+
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+    .sub-headline {
+      font-size: 2rem;
+      font-weight: 700;
+      line-height: 0.95;
+      letter-spacing: 0.04rem;
+      text-transform: uppercase;
+
+      @media screen and (max-width: 939px) and (min-width: 767px),
+        screen and (max-width: 766px) {
+        font-size: 1.3333333333rem;
+      }
+    }
+
+    .text {
+      margin: 20px 0 0;
+      color: #4a4a5a;
+      text-align: center;
+      line-height: 1.2;
+
+      @media screen and (max-width: 939px) and (min-width: 767px),
+        screen and (max-width: 766px) {
+        margin: 16px 0 0;
+        font-size: 0.7777777778rem;
+      }
+    }
+  }
+`;
+
+export default Card;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import { useRouter } from "next/router";
+import styled from "styled-components";
 import MainContent from "@/components/MainContent";
 import CardContent from "@/components/CardContent";
-import styled from "styled-components";
 import main from "@/assets/main/main_image.png";
+import { customerMainContentList } from "@/constants/customer";
 
 const Home = () => {
   const router = useRouter();
   const navigateToMakeatPage = () => router.push("/makeat");
+  const navigateToCustomerPage = () => router.push("/customer");
 
   return (
     <HomeContainer>
@@ -21,12 +23,16 @@ const Home = () => {
             개인화 헬스케어 푸드 서비스
           </>
         }
-        desc="건강을 위한, 나만index을 위한 FOOD, MAKEAT!"
+        desc="건강을 위한, 나만을 위한 FOOD, MAKEAT!"
         onClick={navigateToMakeatPage}
         text="MAKEAT"
         color="main"
       />
-      <CardContent />
+      <CardContent
+        list={customerMainContentList}
+        text="더 알아보기"
+        onClick={navigateToCustomerPage}
+      />
     </HomeContainer>
   );
 };

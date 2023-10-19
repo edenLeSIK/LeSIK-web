344d4365c1d7792b8b9432f391a0a16f788f72d4
EDEN <131233647+edenLeSIK@users.noreply.github.com>
2023-10-19 20:54:32 +0900

    Feature main page (#1)
    * feature favicon
    
    * feature theme color
    
    * style globalStyle
    
    * upload main page image
    
    * style main page layout
    
    * feature Button component
    
    * feature Main content
    
    * feature Card content
    
    * feature Slide Content
    
    * feature Check content
    
    * feature Ads Content

---
+++ src/constants/customer.js
@@ -0,0 +1,56 @@
+import serve from "@/assets/customer/icons/serve.png";
+import plate from "@/assets/customer/icons/plate.png";
+import fork from "@/assets/customer/icons/fork.png";
+import menu from "@/assets/customer/icons/menu.png";
+
+export const customerMainContentList = {
+  label: "For Customer",
+  headline: "초개인화 헬스케어 푸드 서비스",
+  desc: "개인 건강정보를 기반으로 메뉴가 생성되는 맞춤형 건강관리 식단 서비스",
+  button: "MAKEAT 알아보기",
+  contents: [
+    {
+      icon: serve,
+      headline: "언제 어디서나​",
+      text: "모바일 앱을 통해 온﹒오프라인 어디서든 서비스를 이용할 수 있어요.",
+    },
+    {
+      icon: plate,
+      headline: "건강한 삶, 건강한 식사",
+      text: "나만을 위한 건강 맞춤형 음식으로 건강한 삶을 즐겨보세요.",
+    },
+    {
+      icon: fork,
+      headline: "다양하고 맛있는 식사​",
+      text: "건강한 식사도 맛있다! 신선한 재료로 맛있고 다양하게 MAKEAT 하세요.​",
+    },
+    {
+      icon: menu,
+      headline: "카트리지 시스템",
+      text: "AI가 내 건강과 취향을 고려해 언제든 새로운 메뉴를 제시해줘요.",
+    },
+  ],
+};
+
+export const customerPageContentList = {
+  label: "Makeat Customers",
+  headline: "Makeat이 제시해드려요",
+  contents: [
+    {
+      headline: "내게 맞춘 나만을 위한 식사",
+      text: "모든 메뉴는 나의 건강 정보에 기반하여 만들어져요. 기존의 메뉴에서 고르는 것이 아닌, 나만을 위해 새롭게 생성되는 메뉴를 드셔보세요.                ",
+    },
+    {
+      headline: "생성형 푸드 서비스",
+      text: "기존의 메뉴 중에서 골라주는 메뉴가, 그것도 냉동으로 유통되는 메뉴가 과연 나를 위한 건강식일까요?",
+    },
+    {
+      headline: "무한한 메뉴 구성",
+      text: "8종? 12종? 제한된 구성으로 짜여진 식단이 지겨워지는 것은 당연해요. 계속해서 추가되는 블럭 조합으로 무한에 가까운 메뉴를 새롭게 생성해보세요!",
+    },
+    {
+      headline: "어디서든 합리적인 가격에",
+      text: "나만을 위한 블록들의 섬세한 조합의 이면에는 대량으로 생산하여 원가절감을 이룬 블록이 있어요. 븛럭 단위당 생산원가를 낮취 완성된 메뉴의 공급가격을 합리적으로 유지할 수 있어요.",
+    },
+  ],
+};

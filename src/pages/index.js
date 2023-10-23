e2a39881ff3d56ccd1ae124f2d2e82cfb6851718
Eden <edenlesik@gmail.com>
2023-10-23 15:43:17 +0900

    style responsive 939 & 480 & 320 main content

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,17 +27,15 @@ const Home = () => {
           <>
             <span className="cooksup">Cooksup</span>
             <br />
-            완전히 새로운 푸드 솔루션,
+            새로운 푸드 솔루션,
             <br />
-            가장 진보한
-            <br />
-            미래의 푸드서비스 만나보세요
+            미래의 혁신적인 푸드서비스
           </>
         }
         desc={
           <>
             <span className="cooksup letter-spacing">Cooksup</span> 솔루션을
-            통해 구축된 헬스 & 웰니스 푸드 브랜드를 소개합니다
+            통해 구축된 헬스 & 웰니스 푸드 브랜드를 소개합니다.
           </>
         }
         onClick={navigateToMakeatPage}

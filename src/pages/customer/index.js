498b657a603a6ddcadeef63f7f335e3f55b80dba
Eden <edenlesik@gmail.com>
2024-01-17 15:31:43 +0900

    feature customer page setting

--- src/pages/customer/index.js
+++ src/pages/customer/index.js
@@ -1,42 +1,69 @@
 import { useRouter } from "next/router";
+import { useTranslation, Trans } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import styled from "styled-components";
 import MainContent from "@/components/MainContent";
 import DescContent from "@/components/DescContent";
 import CardContent from "@/components/CardContent";
 import background from "@/assets/customer/customer.png";
-import {
-  customerPageContentList,
-  customerMainContentList,
-} from "@/constants/customer";
 
 const Customer = () => {
   const router = useRouter();
+  const { t: mainT } = useTranslation("main");
+  const { t: customerT } = useTranslation("customer");
   const navigateToMakeatPage = () => router.push("/makeat");
+
+  console.log(
+    323232,
+    customerT("customerMainContentList.contents", {
+      returnObjects: true,
+    })
+  );
 
   return (
     <CustomerContainer>
       <MainContent
         image={background}
         headline={
-          <>
-            당신에게 맞춘 건강식단,
-            <br />
-            <span className="makeat">Makeat</span>
-          </>
+          <Trans
+            i18nKey="customer.headline"
+            components={{ span: <span className="makeat" /> }}
+          >
+            {mainT("customer.headline")}
+          </Trans>
         }
-        text={<span className="makeat">Makeat</span>}
+        text={
+          <Trans
+            i18nKey="customer.button"
+            components={{ span: <span className="makeat" /> }}
+          >
+            {mainT("customer.button")}
+          </Trans>
+        }
         onClick={navigateToMakeatPage}
         color="makeat"
       />
       <DescContent
-        label={customerPageContentList.label}
-        headline={customerPageContentList.headline}
-        desc={customerPageContentList.desc}
-        contents={customerPageContentList.contents}
+        label={customerT("customerPageContentList.label")}
+        headline={customerT("customerPageContentList.headline")}
+        contents={customerT("customerPageContentList.contents", {
+          returnObjects: true,
+        })}
       />
-      <CardContent list={customerMainContentList} color="on" />
+      <CardContent
+        list={customerT("customerMainContentList", { returnObjects: true })}
+        color="on"
+      />
     </CustomerContainer>
   );
+};
+
+export const getStaticProps = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["main", "customer"])),
+    },
+  };
 };
 
 const CustomerContainer = styled.main``;

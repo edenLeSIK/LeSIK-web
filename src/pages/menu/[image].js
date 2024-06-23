7fd90d633e5dd344f3a2b8893897664f5b252400
EDEN <edenlesik@gmail.com>
2024-06-23 14:20:53 +0900

    add new images

---
+++ src/pages/menu/[image].js
@@ -0,0 +1,44 @@
+import { useRouter } from "next/router";
+import Image from "next/image";
+import styled from "styled-components";
+import balance1 from "@/assets/menu/balance1.png";
+import balance2 from "@/assets/menu/balance2.png";
+import diet1 from "@/assets/menu/diet1.png";
+import diet2 from "@/assets/menu/diet2.png";
+
+const images = {
+  balance1,
+  balance2,
+  diet1,
+  diet2,
+};
+
+const Menu = () => {
+  const router = useRouter();
+  const { image } = router.query;
+
+  if (image && images[image]) {
+    return (
+      <Container>
+        <Image src={images[image]} alt={image} />
+      </Container>
+    );
+  }
+};
+
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: auto;
+
+  img {
+    width: 100%;
+    max-width: 430px;
+    height: auto;
+  }
+`;
+
+export default Menu;
